b04a54efea0d63472db43e2e77f4399429a5ec1c
biswa418 <biswajeetsutar418@gmail.com>
2023-04-13 04:05:59 +0530

    revert changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  //secrets
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
