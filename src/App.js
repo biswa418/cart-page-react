661b6aa30e885589dbcb6694a229443f8ebebdb3
biswa418 <biswajeetsutar418@gmail.com>
2023-04-13 04:19:59 +0530

    Restore firebase functions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
+import { collection, onSnapshot, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from './index';
+import Loader from "./Loader";
+import './button.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [{
-        price: 19999,
-        title: 'Watch',
-        qty: 2,
-        img: 'https://images.pexels.com/photos/5081914/pexels-photo-5081914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        id: 1
-      },
-      {
-        price: 30999,
-        title: 'Smartphone',
-        qty: 1,
-        img: 'https://images.unsplash.com/flagged/photo-1594344141311-8ea00ba55612?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        id: 2
-      },
-      {
-        price: 10999,
-        title: 'Headphone',
-        qty: 2,
-        img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        id: 3
-      }
-      ]
+      products: [],
+      isLoading: true
     }
+    this.unsubscribe = null;
   }
 
-  handleIncreaseQty = (product) => {
+  async componentDidMount() {
+    // await getDocs(collection(db, "products"))
+    //   .then((snapshot) => {
+    //     const newData = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+    //     this.setState({
+    //       products: newData,
+    //       isLoading: false
+    //     });
+    //   });
+
+    this.unsubscribe = onSnapshot(collection(db, "products")
+      , (snapshot) => {
+        const newData = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+        this.setState({
+          products: newData,
+          isLoading: false
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  handleIncreaseQty = async (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty++;
+    // products[index].qty++;
 
-    this.setState({
-      products // shorthand for products: products
-    });
+    // this.setState({
+    //   products // shorthand for products: products
+    // });
+
+    try {
+      await updateDoc(doc(db, 'products', products[index].id), {
+        qty: products[index].qty + 1
+      });
+    } catch (err) {
+      console.log('Error', err);
+    }
   }
 
-  handleDecreaseQty = (product) => {
+  handleDecreaseQty = async (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -50,20 +69,34 @@ class App extends React.Component {
       return;
     }
 
-    products[index].qty--;
+    // products[index].qty--;
 
-    this.setState({
-      products // shorthand for products: products
-    });
+    // this.setState({
+    //   products // shorthand for products: products
+    // });
+
+    try {
+      await updateDoc(doc(db, 'products', products[index].id), {
+        qty: products[index].qty - 1
+      });
+    } catch (err) {
+      console.log('Error', err);
+    }
   }
 
-  handleDeleteItem = (id) => {
-    const { products } = this.state;
-    const items = products.filter((item) => id !== item.id);
+  handleDeleteItem = async (id) => {
+    // const { products } = this.state;
+    // const items = products.filter((item) => id !== item.id);
 
-    this.setState({
-      products: items
-    });
+    // this.setState({
+    //   products: items
+    // });
+
+    try {
+      await deleteDoc(doc(db, 'products', id));
+    } catch (err) {
+      console.log('Error', err);
+    }
   }
 
   getCartCount = () => {
@@ -88,8 +121,22 @@ class App extends React.Component {
     return price;
   }
 
+  addanItem = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "products"), {
+        price: 21999,
+        qty: 1,
+        title: 'Washing Machine',
+        img: 'https://whirlpoolindia.vtexassets.com/arquivos/ids/164386/Xpert-care-Silver-lid-open-O3-6.5kg_1500x1500.jpg?v=638000290210600000'
+      });
+    } catch (err) {
+      console.log('Error in adding', err);
+    }
+    // console.log("Document written with ID: ", docRef.id);
+  }
+
   render() {
-    const { products } = this.state;
+    const { products, isLoading } = this.state;
 
     return (
       <div className="App">
@@ -102,10 +149,17 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQty}
           onDeleteItem={this.handleDeleteItem}
         />
+        {isLoading && <Loader />}
 
-        <div style={{ padding: 10, fontSize: 25 }}>
-          Total - {this.totalPrice()}
-        </div>
+        {!isLoading &&
+          <>
+            <div style={{ padding: 10, fontSize: 25 }}>
+              Total - {this.totalPrice()}
+            </div>
+
+            <button className="btn-grad" onClick={this.addanItem}>Add an Item</button>
+          </>
+        }
       </div>
     );
   }
---
+++ src/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loader.css';
+
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </div>
+    )
+};
+
+
+export default Loader;
